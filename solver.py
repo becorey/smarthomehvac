9cb31d05c6af30c3495f37d8c6f95d5356a4176e
Corey Berman <becorey@users.noreply.github.com>
2017-03-27 17:58:42 -0500

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,24 @@
+
+
+class Solver(object):
+    #Takes data about thermal system represented as RC circuit
+    #Runs iterative solver to determine node values
+    #Provides solution set: Temperatures on nodes
+    def __init__(self):
+
+        return
+
+class Node(object):
+    #A single node within a circuit
+    #Contains scalar value like temperature or humidity
+    def __init__(self):
+        return
+
+class Component(object):
+    #A single component within a circuit
+    #Represents thermal resistance or capacitance
+    def __init__(self):
+        return
+
+if __name__ == '__main__':
+    print("main")
